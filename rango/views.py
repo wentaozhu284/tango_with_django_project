53afc7325f804e100cb19e4561d37bc36b419f1c
Wentao Zhu <2841135z@student.gla.ac.uk>
2025-02-14 14:10:28 +0000

    chapter10

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout  
+from datetime import datetime
 
 
 
@@ -13,6 +14,27 @@ from django.http import HttpResponse
 from rango.forms import CategoryForm  
 from django.shortcuts import redirect
 from django.urls import reverse
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    
+ 
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1 
+        request.session['last_visit'] = str(datetime.now())  
+    else:
+        request.session['last_visit'] = last_visit_cookie  
+
+ 
+    request.session['visits'] = visits
 
 @login_required
 def user_logout(request): 
@@ -131,20 +153,31 @@ def show_category(request, category_name_slug):
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+    
     context_dict = {
         'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
         'categories': category_list,
         'pages': page_list
     }
 
+  
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+ 
     return render(request, 'rango/index.html', context=context_dict)
 
 
+
+
 def about(request):
+    visitor_cookie_handler(request)  # 先调用此函数，确保 session 存在 visits 数据
+    visits = request.session.get('visits', 1)  # 从 session 获取访问次数
+
     print(request.method)
     print(request.user)
     
-    return render(request, 'rango/about.html', {})
+    return render(request, 'rango/about.html', context={'visits': visits})
 
 
 @login_required
